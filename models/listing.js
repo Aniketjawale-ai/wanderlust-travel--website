7d4997d6991a9f1aba2c2d0020f338e52240c988
Aniket <jawleaniket7@gmail.com>
2026-08-07 10:10:12 +0530

    initial commit

---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema  = mongoose.Schema;
+
+const listingSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default:"https://images.unsplash.com/photo-1513407030348-c983a97b98d8?auto=format&fit=crop&w=1200&q=80",
+        set: (v) =>
+            v === ""
+                ? "https://images.unsplash.com/photo-1513407030348-c983a97b98d8?auto=format&fit=crop&w=1200&q=80 "
+                : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
