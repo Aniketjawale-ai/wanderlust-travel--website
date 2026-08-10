2c9fb179a46815c2fafb58f39851bea65b056778
Aniket <jawleaniket7@gmail.com>
2026-08-10 16:49:31 +0530

    add simple review system

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema } =require("./schema.js");
 const { error } = require("console");
+const Review = require("./models/review.js");
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -127,7 +128,28 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings");
-}));
+})
+);
+
+//Reviews
+//post Route
+
+app.post("/listings/:id/reviews", async (req, res) =>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    
+    
+
+
+    res.redirect(`/listings/${listing._id}`)
+
+});
     
 
 app.all("/{*splat}", (req, res, next) => {
