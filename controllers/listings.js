6ec7d9b1f5b819f5b18593da58b3ecf2d1d8b4d3
Aniket <jawleaniket7@gmail.com>
2026-08-17 12:38:11 +0530

    image preview for edit page

--- controllers/listings.js
+++ controllers/listings.js
@@ -59,8 +59,10 @@ module.exports.renderEditForm = async (req, res) =>{
         req.flash("error", "Listing you requested for does not exitsts");
         return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs", { listing });
-
+    let originalImageUrl = listing.image.url;
+     originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300");
+    
+    res.render("listings/edit.ejs", { listing , originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
